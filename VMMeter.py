24f974b4d09a2500af60ba083e09009fa326d1f0
leonardo329cf <leonardo329cf@gmail.com>
2020-06-22 16:20:04 -0300

    V3

--- VMMeter.py
+++ VMMeter.py
@@ -31,7 +31,7 @@ with open('benchmark.csv', 'w', newline='') as file:
 
     rowToWrite = []
     for i in domNames:
-        headers = ["Mesurament Time(ns)", "cpu time(ns)", "system time(ns)", "user time(ns)", "total alocated(B)", "unused(B)", "available(B)", "usable(B)", "disk caches(B)"]
+        headers = ["Mesurament Time(ns)", "Cpu time(ns)", "System time(ns)", "User time(ns)", "Memory alocated(B)", "Memory Unused(B)", "Memory available(B)", "Memory usable(B)", "Disk caches(B)"]
         rowToWrite.extend(headers)
     writer.writerow(rowToWrite)
 
